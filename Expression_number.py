4c7a4e3f8ee757308e0c86e1adb500803ab9c8df
arrack9 <pingyaoc9@gmail.com>
2018-01-16 16:25:11 +0800

    Expression_number, 浮點數運算

---
+++ Expression_number.py
@@ -0,0 +1,31 @@
+import decimal
+print(2+2)
+print(50 - 5*6)
+print((50 - 5*6)/4)
+a = 8.0/5.0
+print(a)
+b = 8.0//5.0  
+print(b)
+c = 10.0/3.0
+print(c)
+d = 10.0//3.0
+print(d)
+e = 5 ** 3
+print(e)
+f = 7 ** 64
+print(f)
+g = 8 ** 100
+print(g)
+width = 4 * 2
+hight = 6 * 2
+print(width * hight)
+h = 3 * 3.75 / 1.5
+print(h)
+i = repr(1.0 - 0.8)
+print(i)
+j = str(1.0 - 0.8)
+print(j)
+k = decimal.Decimal('1.0')
+l = decimal.Decimal('0.8')
+print(k - l)
+
